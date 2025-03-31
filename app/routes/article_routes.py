361b7ddaf53c4ece7c140876c2af81005bdb9690
Tomas <discipleartem@gmail.com>
2025-03-31 22:55:44 +0300

    fix update Article

--- app/routes/article_routes.py
+++ app/routes/article_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from werkzeug.exceptions import abort
 
@@ -7,29 +7,37 @@ from app.forms.article_forms import ArticleForm
 from app.forms.comment_form import CommentForm
 from app.models.comment import Comment
 
+# Создаем Blueprint для маршрутов, связанных со статьями
 bp = Blueprint('articles', __name__)
 
 @bp.route('/')
 def index():
+    """Перенаправление с корневого маршрута на список статей"""
     return redirect(url_for('articles.list_articles'))
 
 @bp.route('/articles')
 def list_articles():
+    """Отображение списка всех статей"""
     articles = Article.get_all()
     return render_template('articles/list.html', articles=articles)
 
 @bp.route('/article/<int:article_id>')
 def view_article(article_id):
+    """
+    Просмотр отдельной статьи
+    Также отображает форму комментариев и список существующих комментариев
+    """
     article = Article.get_by_id(article_id)
     if article is None:
-        abort(404)
+        abort(404)  # Возвращаем 404, если статья не найдена
     form = CommentForm()
     comments = Comment.get_by_article_id(article_id)
     return render_template('articles/view.html', article=article, comments=comments, form=form)
 
 @bp.route('/article/new', methods=['GET', 'POST'])
-@login_required
+@login_required  # Требуется аутентификация для создания статьи
 def new_article():
+    """Создание новой статьи"""
     form = ArticleForm()
     if form.validate_on_submit():
         Article.create(form.title.data, form.content.data, current_user.id)
@@ -37,10 +45,13 @@ def new_article():
         return redirect(url_for('articles.list_articles'))
     return render_template('articles/form.html', form=form)
 
-
 @bp.route('/article/<int:article_id>/edit', methods=['GET', 'POST'])
-@login_required
+@login_required  # Требуется аутентификация для редактирования
 def edit_article(article_id):
+    """
+    Редактирование существующей статьи.
+    Проверяет права доступа перед редактированием
+    """
     article = Article.get_by_id(article_id)
     if not article:
         flash('Article not found', 'danger')
@@ -51,15 +62,18 @@ def edit_article(article_id):
 
     form = ArticleForm(obj=article)
     if form.validate_on_submit():
-        article.update_article(form.title.data, form.content.data)
+        article.update(form.title.data, form.content.data)
         flash('Article updated successfully!', 'success')
         return redirect(url_for('articles.view_article', article_id=article.id))
     return render_template('articles/form.html', form=form, article=article)
 
-
 @bp.route('/article/<int:article_id>/delete', methods=['POST'])
-@login_required
+@login_required  # Требуется аутентификация для удаления
 def delete_article(article_id):
+    """
+    Удаление статьи.
+    Проверяет права доступа перед удалением
+    """
     article = Article.get_by_id(article_id)
     if article.user_id != current_user.id:
         flash('You are not authorized to delete this article', 'danger')
